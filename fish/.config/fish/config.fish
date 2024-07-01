b9f5cce193dbf0690d76f99ecbe9e2c3aaffb220
Garrett <gmoseke@protonmail.com>
2024-07-01 08:40:46 +0200

    feat(fish): add OS-specific config so I don't have to do stupid branch things

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,15 +8,25 @@ end
 # starship
 starship init fish | source
 
-set --global --export MANPAGER batcat
 set --global --export EDITOR nvim
 
 # ===== Convenience Aliases =====
 # ensure lsd is installed: https://github.com/lsd-rs/lsd
 alias ls="lsd -l"
-alias code="codium"
 alias crabrave="open 'https://youtu.be/LDU_Txk06tM?t=28'"
 alias vim="nvim"
-alias docker="podman"
-alias cat="batcat"
 alias loc="tokei"
+
+# OS-specific config
+switch (uname)
+    case Linux
+        # bat binary is batcat on debian systems
+        set --global --export MANPAGER batcat
+        alias cat="batcat"
+        alias code="codium"
+        alias docker="podman"
+    case Darwin
+        alias cat="bat"
+    case '*'
+        echo Unconfigured OS
+end
