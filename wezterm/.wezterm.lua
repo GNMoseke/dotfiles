6316d4a1f6fb1205425a41dd9509aea5568e682f
Garrett Moseke <gmoseke@protonmail.com>
2024-07-17 08:57:29 -0600

    feat(wezterm): tweak opacity and font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,9 +2,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.enable_tab_bar = false
-config.window_background_opacity = 0.75
-config.font_size = 10
+config.window_background_opacity = 0.85
+config.font_size = 12
 config.default_cursor_style = 'SteadyBar'
 
 return config
