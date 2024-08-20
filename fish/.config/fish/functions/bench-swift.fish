ef4afea49e663360025d2a1ff3eeaa5c3b2eeb09
Garrett Moseke <gmoseke@protonmail.com>
2024-08-20 18:24:09 +0200

    fix(fish): pass through argv to bench-swift function

--- fish/.config/fish/functions/bench-swift.fish
+++ fish/.config/fish/functions/bench-swift.fish
@@ -1,6 +1,6 @@
 function bench-swift
     if [ $(swift package describe --type json | jq -c '.dependencies[] | select(.identity == "package-benchmark")') ]
-        swift package --disable-sandbox benchmark
+        swift package --disable-sandbox benchmark $argv
     else
         echo "No package-benchmark dependency found or not a swift package"
     end
