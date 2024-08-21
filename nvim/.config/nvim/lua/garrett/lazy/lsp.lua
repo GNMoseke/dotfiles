825e033e4d0ddfb0238cf395e4efd68a54e427d0
Garrett Moseke <gmoseke@protonmail.com>
2024-08-21 09:09:29 +0200

    fix(nvim): always enable type inlay on buffer attach

--- nvim/.config/nvim/lua/garrett/lazy/lsp.lua
+++ nvim/.config/nvim/lua/garrett/lazy/lsp.lua
@@ -39,10 +39,8 @@ return {
             })
           end
 
-          if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-            end, '[T]oggle Inlay [H]ints')
+          if client and client.server_capabilities.inlayHintProvider then
+              vim.lsp.inlay_hint.enable(true)
           end
         end,
       })
