6bcc792da2cf20977fb28383866622f910f56001
Garrett <gmosedev@proton.me>
2024-08-26 12:36:11 +0200

    feat(nvim): add todo comment keymaps for trouble/telescope

--- nvim/.config/nvim/lua/garrett/lazy/todo-comments.lua
+++ nvim/.config/nvim/lua/garrett/lazy/todo-comments.lua
@@ -3,5 +3,11 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
         exclude = { "md" }
-    }
+    },
+    config = function()
+        require('todo-comments').setup({})
+
+        vim.keymap.set('n', '<leader>TT', "<CMD>Trouble todo toggle<CR>")
+        vim.keymap.set('n', '<leader>ft', "<CMD>TodoTelescope<CR>")
+    end
 }
