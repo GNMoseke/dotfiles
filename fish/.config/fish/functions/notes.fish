b075cb93b9a34edb2bf8318994f13c1f51c089f2
Garrett <gmosedev@proton.me>
2024-10-13 08:43:23 +0200

    feat(fish): add notes script

---
+++ fish/.config/fish/functions/notes.fish
@@ -0,0 +1,28 @@
+# check if a note for today already exists and open it if so
+
+function notes
+    set -l file_name (date +"%Y-%m-%d").md
+    set -l notes_dir ~/Notes/
+    set -l today_note_path "$notes_dir$file_name"
+
+    set -l config_file_dir ~/.config/notes
+
+    # only have to create if it doesn't exist
+    if not test -e $today_note_path
+        set -l day_of_week (date +"%A")
+
+        # try templates in order of decreasing specificity
+        # note to future self: using >> here so I don't blast away existing content by accident
+        if test -e "$config_file_dir/$day_of_week.md"
+            cat "$config_file_dir/$day_of_week.md" >> $today_note_path
+        else if test (date +"%u") -gt 5; and test -e "$config_file_dir/Weekend.md"
+            cat "$config_file_dir/Weekend.md" >> $today_note_path
+        else if test (date +"%u") -le 5; and test -e "$config_file_dir/Weekday.md"
+            cat "$config_file_dir/Weekday.md" >> $today_note_path
+        else
+            cat "$config_file_dir/Default.md" >> $today_note_path
+        end
+    end
+    $EDITOR $today_note_path
+end
+
