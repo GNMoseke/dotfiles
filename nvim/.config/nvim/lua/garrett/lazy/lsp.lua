0874f28344aeab1f3f956bedb87909a984336ece
Garrett Moseke <gmosedev@proton.me>
2024-11-14 16:20:54 +0100

    chore(nvim): clean plugins

--- nvim/.config/nvim/lua/garrett/lazy/lsp.lua
+++ nvim/.config/nvim/lua/garrett/lazy/lsp.lua
@@ -1,45 +1,49 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      { 'j-hui/fidget.nvim', opts = {} },
-    },
-    config = function()
-      local lspconfig = require('lspconfig')
-
-      lspconfig.sourcekit.setup {
-        capabilities = {
-          workspace = {
-            didChangeWatchedFiles = {
-              dynamicRegistration = true,
-            },
-          },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            { 'j-hui/fidget.nvim', opts = {} },
         },
-      }
-      lspconfig.rust_analyzer.setup {}
+        config = function()
+            local lspconfig = require('lspconfig')
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        desc = "LSP Actions",
-        callback = function(args)
-          local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = args.buf, desc = 'LSP: ' .. desc })
-          end
+            lspconfig.sourcekit.setup {
+                capabilities = {
+                    workspace = {
+                        didChangeWatchedFiles = {
+                            dynamicRegistration = true,
+                        },
+                    },
+                },
+            }
+            lspconfig.rust_analyzer.setup {}
+            lspconfig.bashls.setup {}
+            lspconfig.dockerls.setup {}
+            lspconfig.terraformls.setup {}
+            lspconfig.lua_ls.setup {}
 
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-          map('<leader>th', function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          end, '[T]oggle Inlay [H]ints')
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = "LSP Actions",
+                callback = function(args)
+                    local map = function(keys, func, desc)
+                        vim.keymap.set('n', keys, func, { buffer = args.buf, desc = 'LSP: ' .. desc })
+                    end
+
+                    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+                    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+                    map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+                    map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+                    map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+                    map('K', vim.lsp.buf.hover, 'Hover Documentation')
+                    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                    map('<leader>th', function()
+                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+                    end, '[T]oggle Inlay [H]ints')
+                end,
+            })
         end,
-      })
-    end,
-  },
+    },
 }
