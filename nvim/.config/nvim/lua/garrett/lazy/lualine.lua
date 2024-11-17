52a3c5f0735290354322fe0ed6d88ebd6f07ac47
Garrett Moseke <gmosedev@proton.me>
2024-11-17 17:34:00 +0100

    refactor(nvim): add icons to lualine for mode sleection, tweak filepath setting

--- nvim/.config/nvim/lua/garrett/lazy/lualine.lua
+++ nvim/.config/nvim/lua/garrett/lazy/lualine.lua
@@ -5,6 +5,25 @@ return {
         require('lualine').setup {
             options = {
                 theme = 'catppuccin'
+            },
+            sections = {
+                lualine_a = {
+                    { 'mode', fmt = function(str)
+                        local mode = str:lower()
+                        if mode == "normal" then
+                            return " " .. str
+                        elseif mode == "insert" then
+                            return " " .. str
+                        elseif mode == "visual" or mode == "v-line" or mode == "v-block" then
+                            return " " .. str
+                        else
+                            return str
+                        end
+                    end }
+                },
+                lualine_c = {
+                    { 'filename', path = 3 }
+                }
             }
         }
     end
