603725d703402a7614201b88aa04b140b6d98be0
Garrett Moseke <gmosedev@proton.me>
2024-11-17 17:34:16 +0100

    refactor(nvim): use nerd font icons for diagnostic signs

--- nvim/.config/nvim/lua/garrett/lazy/lsp.lua
+++ nvim/.config/nvim/lua/garrett/lazy/lsp.lua
@@ -22,6 +22,12 @@ return {
             lspconfig.terraformls.setup {}
             lspconfig.lua_ls.setup {}
 
+            local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+            end
+
             vim.api.nvim_create_autocmd('LspAttach', {
                 desc = "LSP Actions",
                 callback = function(args)
