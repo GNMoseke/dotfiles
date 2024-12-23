c45911a9448338595641341840faf3bb88452319
Garrett Moseke <gmosedev@proton.me>
2024-12-23 09:27:51 +0100

    fix(wezterm): add os-specific tweaks

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,9 +5,16 @@ config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.enable_tab_bar = false
 config.window_background_opacity = 0.85
-config.macos_window_background_blur = 15
-config.font_size = 10
 config.default_cursor_style = 'SteadyBar'
 config.window_decorations = "RESIZE"
+
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+    config.font_size = 12
+    config.macos_window_background_blur = 15
+else
+    config.initial_cols = 220
+    config.initial_rows = 80
+    config.font_size = 10
+end
 
 return config
