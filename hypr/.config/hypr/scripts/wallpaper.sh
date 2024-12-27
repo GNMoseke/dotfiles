c2cb8be11d32346e0460f5d648d207239823abc2
Garrett Moseke <gmosedev@proton.me>
2024-12-27 19:32:17 +0100

    chore: general cleanup

--- hypr/.config/hypr/scripts/wallpaper.sh
+++ hypr/.config/hypr/scripts/wallpaper.sh
@@ -3,6 +3,6 @@
 # credit to https://github.com/hyprwm/hyprpaper/issues/194#issuecomment-2324632187
  
 # Pick random wallpaper
-random_pick=$(find "/home/garrett/Pictures/Wallpaper/" -type f -name "*.png" -o -name "*.jpg" | shuf -n1)
+random_pick=$(find "$HOME/Pictures/Wallpaper/" -type f -name "*.png" -o -name "*.jpg" | shuf -n1)
 # Replace the placeholder in template and save to .conf file
 sed -e "s~<wp>~${random_pick}~g" ~/.config/hypr/templates/hyprpaper.template > ~/.config/hypr/hyprpaper.conf
