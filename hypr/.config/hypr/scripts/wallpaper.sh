048c9b2264d3493c3212e4f502550418eac919f5
Garrett Moseke <gmosedev@proton.me>
2024-12-29 21:30:34 +0100

    feat(hyprland): better wallpaper script and add a bind to shuffle

--- hypr/.config/hypr/scripts/wallpaper.sh
+++ hypr/.config/hypr/scripts/wallpaper.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
+random_background=$(find "$HOME/Pictures/Wallpaper/" -type f -name "*.png" -o -name "*.jpg" | shuf -n 1)
 
-# credit to https://github.com/hyprwm/hyprpaper/issues/194#issuecomment-2324632187
- 
-# Pick random wallpaper
-random_pick=$(find "$HOME/Pictures/Wallpaper/" -type f -name "*.png" -o -name "*.jpg" | shuf -n1)
-# Replace the placeholder in template and save to .conf file
-sed -e "s~<wp>~${random_pick}~g" ~/.config/hypr/templates/hyprpaper.template > ~/.config/hypr/hyprpaper.conf
+hyprctl -q hyprpaper unload all
+hyprctl -q hyprpaper preload $random_background
+hyprctl -q hyprpaper wallpaper "DP-3, $random_background"
+
