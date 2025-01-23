224b7c1a0bb678882060348f92bd86a32886e98e
Garrett Moseke <gmosedev@proton.me>
2025-01-23 09:44:25 +0100

    fix(wezterm): use proportional font variation for clearer symbols

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font Propo")
 config.enable_tab_bar = false
 config.window_background_opacity = 0.85
 config.default_cursor_style = 'SteadyBar'
