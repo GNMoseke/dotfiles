62d56d0f82fb7ff7f1a61e773214e9ee038811c3
Garrett Moseke <gmosedev@proton.me>
2025-03-11 10:06:20 +0100

    feat(nvim): add MARK keyword to todo-comments

--- nvim/.config/nvim/lua/garrett/lazy/todo-comments.lua
+++ nvim/.config/nvim/lua/garrett/lazy/todo-comments.lua
@@ -2,10 +2,15 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
-        exclude = { "md" }
+        exclude = { "md" },
     },
     config = function()
-        require('todo-comments').setup({})
+        require('todo-comments').setup({
+            keywords = {
+                MARK = { icon = " ", color = "hint", alt = { "MARK" } },
+            },
+            merge_keywords = true
+        })
 
         vim.keymap.set('n', '<leader>TT', "<CMD>Trouble todo toggle<CR>")
         vim.keymap.set('n', '<leader>ft', "<CMD>TodoTelescope<CR>")
