2e712f48cffda58e0b4a6f1b77cc358693765410
Garrett Moseke <gmosedev@proton.me>
2025-03-20 20:40:09 +0100

    feat(nvim): add obsidian plugin

--- nvim/.config/nvim/lua/garrett/init.lua
+++ nvim/.config/nvim/lua/garrett/init.lua
@@ -6,15 +6,26 @@ local augroup = vim.api.nvim_create_augroup
 local autocommand = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
+-- Stuff that I want to override for specific md files (obsidian-style)
+local note_overrides = augroup('NoteOverrides', {})
+
 autocommand('TextYankPost', {
     group = yank_group,
     pattern = '*',
     callback = function()
-	vim.highlight.on_yank({
-	    higroup = 'IncSearch',
-	    timeout = 40,
-	})
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
     end,
+})
+
+autocommand('BufReadPost', {
+    group = note_overrides,
+    pattern = os.getenv('HOME') .. "/Documents/Notes/*.md",
+    callback = function()
+        vim.opt.conceallevel = 1
+    end
 })
 
 vim.cmd.colorscheme "catppuccin"
