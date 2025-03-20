ef96e33846fe2caee640e28267c0ec3676d6533c
Garrett Moseke <gmosedev@proton.me>
2025-03-20 16:51:40 +0100

    refactor(nvim): clean up some formatting, add a couple treesitter md reqs

--- nvim/.config/nvim/lua/garrett/lazy/dap.lua
+++ nvim/.config/nvim/lua/garrett/lazy/dap.lua
@@ -21,7 +21,7 @@ return {
                 program = function()
                     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
                 end,
-                 -- FIXME: this is funky
+                -- FIXME: this is funky
                 cwd = '${workspaceFolder}',
                 stopOnEntry = false,
                 args = {},
@@ -36,33 +36,33 @@ return {
                 program = function()
                     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
                 end,
-                 -- FIXME: this is funky
+                -- FIXME: this is funky
                 cwd = '${workspaceFolder}',
                 stopOnEntry = false,
                 args = {},
             },
-              initCommands = function()
-              -- Find out where to look for the pretty printer Python module
-              local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+            initCommands = function()
+                -- Find out where to look for the pretty printer Python module
+                local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
 
-              local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
-              local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+                local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+                local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
 
-              local commands = {}
-              local file = io.open(commands_file, 'r')
-              if file then
-                for line in file:lines() do
-                  table.insert(commands, line)
+                local commands = {}
+                local file = io.open(commands_file, 'r')
+                if file then
+                    for line in file:lines() do
+                        table.insert(commands, line)
+                    end
+                    file:close()
                 end
-                file:close()
-              end
-              table.insert(commands, 1, script_import)
+                table.insert(commands, 1, script_import)
 
-              return commands
+                return commands
             end,
         }
-        
-        vim.fn.sign_define('DapBreakpoint', {text='', texthl='', linehl='', numhl=''})
+
+        vim.fn.sign_define('DapBreakpoint', { text = '', texthl = '', linehl = '', numhl = '' })
         vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
         vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
 
--- nvim/.config/nvim/lua/garrett/lazy/treesitter.lua
+++ nvim/.config/nvim/lua/garrett/lazy/treesitter.lua
@@ -3,12 +3,11 @@ return {
     build = ":TSUpdate",
     config = function()
         require('nvim-treesitter.configs').setup({
-            ensure_installed = { "c", "lua", "rust", "fish", "bash", "swift" },
+            ensure_installed = { "c", "lua", "rust", "fish", "bash", "swift", "markdown", "markdown_inline" },
             sync_install = false,
             auto_install = true,
-            ignore_install = { "javascript", "typescript" },
-            indent = { enable = true},
-            highlight = { 
+            indent = { enable = true },
+            highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = { 'markdown' }
             }
