f86b4f7e3151fd8fba517f991c64ee6cc5d224d5
Garrett Moseke <gmosedev@proton.me>
2025-05-02 12:28:00 +0200

    fix(nvim): hack to workaround double border floats on 0.11

--- nvim/.config/nvim/lua/garrett/init.lua
+++ nvim/.config/nvim/lua/garrett/init.lua
@@ -28,4 +28,19 @@ autocommand('BufReadPost', {
     end
 })
 
+-- FIXME: this hacks around double borders in telescope. can be removed
+-- after https://github.com/nvim-lua/plenary.nvim/pull/649
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TelescopeFindPre",
+  callback = function()
+    vim.opt_local.winborder = "none"
+    vim.api.nvim_create_autocmd("WinLeave", {
+      once = true,
+      callback = function()
+        vim.opt_local.winborder = "rounded"
+      end,
+    })
+  end,
+})
+
 vim.cmd.colorscheme "catppuccin"
