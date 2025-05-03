b800865474bdb173d63001b49b36e559995f1b0c
Garrett Moseke <gmosedev@proton.me>
2025-05-03 16:49:13 +0200

    feat(fish): update "today" func for word van vaandag

--- fish/.config/fish/functions/today.fish
+++ fish/.config/fish/functions/today.fish
@@ -1,8 +1,27 @@
 # Requires:
 # https://github.com/race604/clock-tui
 # https://github.com/newsboat/newsboat
+# https://github.com/ericchiang/pup
 function today
     tmux split-window -h "flashcards"
     tclock -c Magenta timer -d 10m
-    newsboat
+    echo -e "\033[1;35m === HET (WILLEKEURIG) WORD VAN VANDAAG === \033[0m"
+    set -l wvv (curl -s -L https://www.woordvanvandaag.nl/random | \
+        pup 'p:nth-child(7) text{}' | \
+        tr -d '\n' | tr -s '\t')
+    set -l components (string split '=' -- $wvv)
+    set -l nl (string trim -- $components[1])
+    set -l en (string trim -- $components[2])
+    echo -e "\033[0;32m$nl\033[0m"
+    echo -e "\033[0;34m$en\033[0m"
+
+    set -l csv "$nl,$en"
+    set -l exists (rg -F "$csv" ~/dotfiles/flashcards/.athenaeum/word_van_vandaag.csv)
+    if test -n "$exists" 
+        echo "Already in flashcard set!"
+    else
+        echo "$csv" >> ~/dotfiles/flashcards/.athenaeum/word_van_vandaag.csv
+        echo "Added to flashcard set!"
+    end
+    newsboat --quiet
 end
