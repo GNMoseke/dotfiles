20ef0cf114023ca1e36489d212ccc32a66273ce9
Garrett Moseke <gmosedev@proton.me>
2025-06-14 16:06:53 +0200

    fix(bin): increase cpu temp alert to 96 degrees

--- bin/.local/bin/alert-cpu-temp.sh
+++ bin/.local/bin/alert-cpu-temp.sh
@@ -2,9 +2,9 @@
 
 while true; do
     curr_temp=$(cat /sys/class/hwmon/hwmon3/temp1_input)
-    if (( $curr_temp >= 95000 )); then
-        notify-send "CPU Temp Above 95°" \
-            --urgency critical \
+    if (( $curr_temp > 96000 )); then
+        notify-send "CPU Temp Above 96°" \
+            --urgency normal \
             --app-name "hwmon" \
             --icon "$(realpath ~/Pictures/Misc/temperature-alert.jpg)" \
             --expire-time 3000 \
