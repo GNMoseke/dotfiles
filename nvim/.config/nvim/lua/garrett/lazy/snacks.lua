dc169aca2ea40a8611b949a3bea536225ba7809d
Garrett Moseke <gmosedev@proton.me>
2025-06-16 15:24:36 +0200

    feat(nvim): enable scratch in snacks.nvim

--- nvim/.config/nvim/lua/garrett/lazy/snacks.lua
+++ nvim/.config/nvim/lua/garrett/lazy/snacks.lua
@@ -2,11 +2,16 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+    keys = {
+        { "<leader>.", function() Snacks.scratch() end,        desc = "Toggle Scratch Buffer" },
+        { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+    },
     ---@type snacks.Config
     opts = {
         bigfile = { enabled = true },
         input = { enabled = true },
         picker = { enabled = true },
         notifier = { enabled = true },
+        scratch = { enabled = true }
     },
 }
