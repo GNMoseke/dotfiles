8027af0623d705419b246578b6ca223e13397464
Garrett Moseke <gmosedev@proton.me>
2025-06-19 17:32:07 +0200

    feat(nvim): add telescope-fzf-native

--- nvim/.config/nvim/lua/garrett/lazy/telescope.lua
+++ nvim/.config/nvim/lua/garrett/lazy/telescope.lua
@@ -15,11 +15,19 @@ return {
                         preview_height = 0.8,
                     },
                 },
+                fzf = {
+                    fuzzy = true,     -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
             }
         })
 
         local builtin = require('telescope.builtin')
         require("telescope").load_extension("undo")
+        require('telescope').load_extension('fzf')
 
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fc', builtin.command_history, {})
