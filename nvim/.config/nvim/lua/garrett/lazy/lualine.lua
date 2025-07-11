d05244d6567a54e79eefc099a2947a964496c05c
Garrett Moseke <gmosedev@proton.me>
2025-07-11 17:52:19 +0200

    nvim: add jj status to lualine

--- nvim/.config/nvim/lua/garrett/lazy/lualine.lua
+++ nvim/.config/nvim/lua/garrett/lazy/lualine.lua
@@ -2,6 +2,18 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local function optional_jj()
+            local filepath = vim.fn.expand('%:p:h')
+            local jjdir = vim.fn.finddir('.jj', filepath .. ';')
+            if jjdir and #jjdir > 0 and #jjdir < #filepath then
+                local handle = io.popen("jj log --revisions @ --no-graph --ignore-working-copy --color never --limit 1 --template powerline_summary")
+                local result = handle:read("*a")
+                handle:close()
+                return result
+            else
+                return ""
+            end
+        end
         require('lualine').setup {
             options = {
                 theme = 'catppuccin'
@@ -21,6 +33,7 @@ return {
                         end
                     end }
                 },
+                lualine_b = { 'branch', 'diff', 'diagnostics', optional_jj },
                 lualine_c = {
                     { 'filename', path = 3 }
                 }
