3c12321c1583af426f26a73e444d9e4ad9adf2e6
Garrett Moseke <gmosedev@proton.me>
2025-08-03 11:10:05 +0200

    fish: flashcards ergonomics

--- fish/.config/fish/functions/flashcards.fish
+++ fish/.config/fish/functions/flashcards.fish
@@ -1,6 +1,11 @@
 # TODO: can remove this when ath supports it as part of the tui
 function flashcards
-    set -l set (cd ~/.athenaeum && lsd --human-readable . | fzf --tmux 80% | sed -e 's/\..*$//')
-    ath --sets-dir ~/.athenaeum/ --set "$set" --shuffle --capitalize
+    set -l set (cd ~/.athenaeum && lsd --human-readable . | fzf | sed -e 's/\..*$//')
+    set -l side (echo -e "front\nreverse" | fzf --preview 'echo {}')
+    if [ "$side" = "reverse" ]
+        ath --sets-dir ~/.athenaeum/ --set "$set" --shuffle --capitalize --reverse
+    else
+        ath --sets-dir ~/.athenaeum/ --set "$set" --shuffle --capitalize
+    end
 end
 
