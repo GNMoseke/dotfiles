eeb9d8b19f4ed80f7e44d84764e160e28b04c62f
Garrett Moseke <gmosedev@proton.me>
2025-08-23 16:53:28 +0200

    wezterm: standardize font size
    I like being able to read

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,12 +8,10 @@ config.adjust_window_size_when_changing_font_size = false
 config.window_background_opacity = 0.88
 config.default_cursor_style = 'SteadyBlock'
 config.window_decorations = "RESIZE"
+config.font_size = 12
 
 if wezterm.target_triple == 'aarch64-apple-darwin' then
-    config.font_size = 12
     config.macos_window_background_blur = 20
-else
-    config.font_size = 10
 end
 
 -- TODO: can be removed once https://github.com/wez/wezterm/issues/5103 included in new wezterm release
