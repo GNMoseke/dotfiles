8b95891e416a24b7b8e6bf42f4bde1d60ef2e822
Garrett Moseke <gmosedev@proton.me>
2025-10-26 08:40:43 +0100

    fish: rename scratchdir -> mkscratch

--- fish/.config/fish/functions/mkscratch.fish
+++ fish/.config/fish/functions/mkscratch.fish
@@ -1,5 +1,5 @@
 # converted to fish and simplified from https://codeberg.org/EvanHahn/dotfiles/src/commit/843b9ee13d949d346a4a73ccee2a99351aed285b/home/zsh/.config/zsh/aliases.zsh#L43-L51
-function scratchdir
+function mkscratch
     cd "$(mktemp -d)"
     chmod -R 0700 .
 end
