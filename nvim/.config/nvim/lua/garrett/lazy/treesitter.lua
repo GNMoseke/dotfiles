ba5837f612805c41d7eca66f23252de8f45f0c7f
Garrett Moseke <gmosedev@proton.me>
2025-11-18 19:06:20 +0100

    nvim: add treesitter config for superMD
    Not convinced I'll stick with this, missing some highlights from normal
    MD I like, but for now it stops annoying render issues.

--- nvim/.config/nvim/lua/garrett/lazy/treesitter.lua
+++ nvim/.config/nvim/lua/garrett/lazy/treesitter.lua
@@ -12,5 +12,33 @@ return {
                 additional_vim_regex_highlighting = { 'markdown' }
             }
         })
+
+        local parser_config = require(
+            'nvim-treesitter.parsers'
+        ).get_parser_configs()
+
+        parser_config.supermd = {
+            install_info = {
+                url = 'https://github.com/kristoff-it/supermd',
+                includes = { 'tree-sitter/supermd/src' },
+                files = {
+                    'tree-sitter/supermd/src/parser.c',
+                    'tree-sitter/supermd/src/scanner.c'
+                },
+                branch = 'main',
+                generate_requires_npm = false,
+                requires_generate_from_grammar = false,
+            },
+            filetype = 'supermd',
+        }
+
+        vim.filetype.add {
+            extension = {
+                smd = 'supermd',
+                --shtml = 'superhtml',
+                --ziggy = 'ziggy',
+                --['ziggy-schema'] = 'ziggy_schema',
+            },
+        }
     end
 }
---
+++ nvim/.config/nvim/queries/supermd/injections.scm
@@ -0,0 +1,13 @@
+;https://github.com/kristoff-it/supermd/blob/main/editors/neovim/queries/supermd/injections.scm
+(fenced_code_block
+  (info_string
+    (language) @injection.language)
+  (code_fence_content) @injection.content)
+
+((html_block) @injection.content (#set! injection.language "html"))
+
+(document . (section . (thematic_break) (_) @injection.content (thematic_break)) (#set! injection.language "ziggy"))
+
+((minus_metadata) @injection.content (#set! injection.language "ziggy"))
+
+((inline) @injection.content (#set! injection.language "supermd_inline"))
